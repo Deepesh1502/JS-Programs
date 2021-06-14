d2059aa48ae1513d35242f53fec8afc596cdd151
Deepesh1502 <63232108+Deepesh1502@users.noreply.github.com>
2021-06-14 22:13:04 +0530

    Create ForLoop.js

---
+++ Loops/ForLoop.js
@@ -0,0 +1,8 @@
+//the For Loop will pint the value of i by increasing by 1
+let i=1;
+for(i=1; i<10; i+=1); {
+  console.log(i);
+  /*This statement will print the value of i
+  Note that the console.log must be writtern in braces
+  to get repeated output*/
+}
