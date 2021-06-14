9134758d55ad70b95000129565766f0ec322b12c
Deepesh1502 <63232108+Deepesh1502@users.noreply.github.com>
2021-06-14 21:49:30 +0530

    Update WhileLoop.js

--- Loops/WhileLoop.js
+++ Loops/WhileLoop.js
@@ -1,7 +1,10 @@
 //Program to display numbers from 1 to 10
 //initialize the variable
 let i=1, n=10;
+//The let keyword assign value to variables by using (=) 
 while(i<=n) {
-  console.log(i);
-  i+=1;
+  console.log(i);//this will print the value of variable (i) till the condition is not met
+  i+=1;//The part i+=1 will increase the value of i by 1
 }
+/*By changing the value of 1 in 7th line to other number
+and increasing the value of n to 100 or 1000 a table can be made*/
