4cee6fc046f8d89db57b74b284ad63906da27120
Deepesh Garg <63232108+Deepesh1502@users.noreply.github.com>
2021-07-09 10:07:30 +0530

    Update Factorial.js

--- Math/Factorial.js
+++ Math/Factorial.js
@@ -1,5 +1,6 @@
-// The logic to get factorial is using IfElse statement with return
-// Return is a loop which execute one time in a program
+// The logic to get factorial is using IfElse
+function Factorial() {
+  let n = parseInt(prompt("Enter Value"));
 function factorial(n) { //We are use factorial as a function
   if (n==0) {
     return 1;
@@ -7,5 +8,6 @@ function factorial(n) { //We are use factorial as a function
     return factorial(n-1)*n; //This will return the value until If condition is not met
   }
 }
-console.log(factorial(4)); //This will print the answer of 4!
-//You can change the 4 into any positive number to get output
+console.log(factorial(n)); //This will print the answer of n!
+}
+Factorial();
