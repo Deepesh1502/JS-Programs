b7572318603ceb3c50b16202818324c2d78db6ca
Deepesh Garg <63232108+Deepesh1502@users.noreply.github.com>
2021-07-14 00:11:07 +0530

    Table.js

--- Math/Table.js
+++ Math/Table.js
@@ -1,13 +1,16 @@
 /*The ForLoop is used to create a table
-In this program you must enter the final value
+In this program you must enter the value of final Multiplicand
 to stop the ForLoop
 */
 function Table() {
   let i=parseInt(prompt("Enter value for table of:"));
-  let j=parseInt(prompt("Enter Final Value:"));
+  let j=parseInt(prompt("Enter value of Final Multiplicand:"));
   let n;
-  for (n=i;n<=(i*10);n+=i) {
-  console.log(n);
+  let a=1;
+  for (n=i;n<=(i*j);n+=i) {
+  console.log(i+" * "+a+" = "+n);
+  //The print statement has string to make output more clear
+  a++;
   }
 }
 Table();
