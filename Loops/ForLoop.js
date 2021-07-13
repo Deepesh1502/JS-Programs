98cc11c7690a31e54833ef8e42b9a4742b794342
Deepesh Garg <63232108+Deepesh1502@users.noreply.github.com>
2021-07-13 13:26:08 +0530

    Create ForLoop.js

---
+++ Loops/ForLoop.js
@@ -0,0 +1,11 @@
+//Program to display numbers
+function ForLoop() {
+//Initialize the First and Last value
+  let i=parseInt(prompt("Enter Initial Value:"));
+  let n=parseInt(prompt("Enter Final Value:"));
+  for (i;i<=n;i++) {
+  console.log(i);
+//It will print i until value of i<=n
+  }
+}
+ForLoop();
