e8c64189790052ea8e7c9c2bf3da81e7d94addf4
Deepesh Garg <63232108+Deepesh1502@users.noreply.github.com>
2021-07-27 19:46:45 +0530

    Create StringExplicit.js

---
+++ Conversion/StringExplicit.js
@@ -0,0 +1,25 @@
+//Convert to String Explicit
+let result;
+//number to string
+result = String(213);
+  console.log(result);
+result = String(1+3);
+  console.log(result);
+    
+//other data types to string
+result = String(null);
+  console.log(result);
+result = String(undefined);
+  console.log(result);
+result = String(NaN);
+  console.log(result);
+result = String(true);
+  console.log(result);
+result = String(false);
+  console.log(result);
+    
+// using toString()
+result = (213).toString();
+  console.log(result);
+result = true.toString();
+  console.log(result);
