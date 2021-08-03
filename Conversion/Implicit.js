6bc0a153c1c645f6a882ea7561e4a3b2ef75c386
Deepesh Garg <63232108+Deepesh1502@users.noreply.github.com>
2021-08-03 09:05:08 +0530

    Update Implicit.js
    The comments are added as results

--- Conversion/Implicit.js
+++ Conversion/Implicit.js
@@ -1,49 +1,49 @@
   let result;  
 //Implicit Conversion to Strings
   result='6'+2; 
-    console.log(result);
+    console.log(result); //62
   result='6'+true; 
-    console.log(result);
+    console.log(result); //6true
   result='6'+undefined; 
-    console.log(result);
+    console.log(result); //6undefined
   result='6'+null; 
-    console.log(result);
+    console.log(result); //6null
   
 //Implicit Conversion to Numbers
   result='6'-2;
-    console.log(result);
+    console.log(result); //4
   result='6'/'2';
-    console.log(result);
+    console.log(result); //3
     
 //Non-numeric String Results to NaN
   result = 'Hello'-'World';
-    console.log(result);
+    console.log(result); //NaN
   result='6'-'Hello';
-    console.log(result);
+    console.log(result); //NaN
   result='6'*'Hello';
-    console.log(result);
+    console.log(result); //NaN
     
 //Implicit Boolean Conversion to Number
 //If Boolean is used, true is 1, false is 0
   result='6'-false;
-    console.log(result);
+    console.log(result); //6
   result=6-true;
-    console.log(result);
+    console.log(result); //5
   result=6-false;
-    console.log(result);
+    console.log(result); //6
     
 //null Conversion to Number
   result=6+null;
-    console.log(result);
+    console.log(result); //6
   result=6-null;
-    console.log(result);
+    console.log(result); //6
 
 //Undefined used with null, Boolean and Number
   result=6+undefined;
-    console.log(result);
+    console.log(result); //NaN
   result=6-undefined;
-    console.log(result);
+    console.log(result); //NaN
   result=true+undefined;
-    console.log(result);
+    console.log(result); //NaN
   result=null+undefined;
-    console.log(result);
+    console.log(result); //NaN
